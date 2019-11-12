be774128c882ac540e7ed1d4898cdf710d7a687e
Stella <feelis1119@naver.com>
2019-11-12 16:30:04 -0500

    update

---
+++ js/modules/DataObject.js
@@ -0,0 +1,31 @@
+export default {
+    Stella: {
+    name: "Stella",
+    role: "Student",
+    bio: "I hate sass, sass deleted my css twice",
+    avatar: "zoolander.jpg"
+    },  
+    
+    Trevor: {
+        name: "Trevor",
+        role: "Professor",
+        bio: "I have sad, sad story",
+        avatar: "justin.jpeg"
+    },
+    
+    Joe: {
+        name: "Joe",
+        role: "Professor",
+        bio: "I like design!",
+        avatar: "pooh.jpeg"
+    },
+
+    Pan: {
+        name: "Pan",
+        role: "Professor",
+        bio: "I'm very strict",
+        avatar: "pan.jpg"
+    }
+
+}
+
